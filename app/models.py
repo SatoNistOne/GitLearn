6309e44c304be059e1001c69b536b0d2aea972a5
SatoNistOne <patronofgoodluck@gmail.com>
2026-03-27 20:27:32 +0300

    v1.2

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(256), nullable=False)
